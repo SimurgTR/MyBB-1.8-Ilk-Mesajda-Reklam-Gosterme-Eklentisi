8ce7d3c2b4ac1d19a833570a372281086d0de104
Hüseyin Körbalta <huseyinkorbalta@gmail.com>
2018-04-14 18:10:46 +0300

    İlk Mesajda Reklam Gösterme Eklenti Dil Dosyası

---
+++ inc/languages/turkish/admin/simurgads.lang.php
@@ -0,0 +1,35 @@
+<?php
+/*
+MyBB 1.8 İlk Mesajda Reklam Gösterme Eklentisi Dil Dosyası v1.0
+Eklenti Yapımcısı: Hüseyin KÖRBALTA (Simurg)
+Website: https://huseyinkorbalta.com - https://mybb.com.tr
+Güncelleme Tarihi: 14.04.2018
+Github: 
+*/
+
+// Eklenti Bilgileri
+$l['simurgads_adi'] = 'İlk Mesajda Reklam Gösterme';
+$l['simurgads_aciklama'] = 'Bu eklenti ile forumunuzda açılan konuların ilk mesajının içerisine reklam alanları oluşturmanıza olanak sağlar.';
+$l['simurgads_web'] = 'https://huseyinkorbalta.com';
+$l['simurgads_yapimci'] = 'Simurg';
+$l['simurgads_yapweb'] = 'https://huseyinkorbalta.com';
+
+// Eklenti Ayarları 
+$l['simurgads_eklentiayarlari'] = 'İlk Mesajda Reklam Gösterme Ayarları';
+$l['simurgads_aciklamasi'] = 'Bu kısımdan ilk mesajda reklam gösterme eklentisinin ayarlarını yapılandırabilirsiniz.';
+$l['simurgads_aktif'] = 'Eklenti Aktif Edilsin mi?';
+$l['simurgads_aktif_aciklama'] = 'Eklentiyi aktif etmek istiyorsanız açık seçeneğini seçiniz.Kapatmak için ise tam tersini seçiniz.';
+$l['simurgads_adskonum'] = 'Reklam Kodları Hangi Konumda Gösterilsin?';
+$l['simurgads_adskonum_aciklama'] = 'Reklam kodlarının konu içerisinde ilk mesajda nerede gösterilmesini istiyorsanız onu seçiniz.';
+$l['simurads_reklamkodu'] = 'Gösterilmesini İstediğiniz Reklam Kodu';
+$l['simurgads_reklamkodu_aciklama'] = 'Gösterilmesini istediğiniz reklam kodlarını buraya ekleyebilirsiniz. Not: Reklam kodlarınızda HTML etiketlerini kullanabilirsiniz. (Google Adsense,Admob vb.)';
+$l['simurgads_ikincireklamkodu'] = 'Gösterilmesini İstediğiniz İkinci Reklam Kodu';
+$l['simurgads_ikincireklamkodu_aciklama'] = 'Gösterilmesini istediğiniz ikinci reklam kodunu buraya ekleyebilirsiniz.Not: Reklam kodlarınızda HTML etiketlerini kullanabilirsiniz.';
+$l['simurgads_reklamgösterimi'] = 'Her Konunun İlk Mesajında Reklam Gösterilsin mi?';
+$l['simurgads_reklamgösterimi_aciklama'] = 'Forumlarda açılan her konunun ilk mesajında reklam gösterilmesini istiyorsanız bu ayarı aktif ediniz.';
+$l['simurgads_forumizinleri'] = 'Reklamlar Hangi Kategorilerde/Forumlarda Gösterilmesin?';
+$l['simurgads_forumizinleri_aciklama'] = 'Reklamlarınızın hangi kategorilerde/forumlarda gösterilmesini istemiyorsanız eğer aşağıda ki kutuya o kategorilerin/forumların ID numaralarını virgül (,) ile ayırarak giriniz. Örnek: 1,2,3';
+$l['simurgads_kullanicizinleri'] = 'Reklamlar Hangi Üye Gruplarına Gösterilmesin?';
+$l['simurgads_kullanicizinleri_aciklama'] = 'Reklamlarınızın hangi üye gruplarına gösterilmesini istemiyorsanız eğer,o üye grubunun ID numaralarını aşağıda ki kutucuya virgül (,) ile ayırarak giriniz.Örnek: 1,2,3 ';
+
+?>
